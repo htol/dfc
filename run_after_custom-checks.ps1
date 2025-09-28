038a7f5d5e77ed628b55f0d7b4e4c4b8202b0c98
htol <github@h-tol.net>
2025-09-29 01:43:03 +0200

    update

--- run_after_custom-checks.ps1
+++ run_after_custom-checks.ps1
@@ -1,1 +1,2 @@
 [Environment]::SetEnvironmentVariable("XDG_CONFIG_HOME", "$env:USERPROFILE\.config", "User")
+[Environment]::SetEnvironmentVariable("GIT_SSH", "C:\WINDOWS\System32\OpenSSH\ssh.exe", "User")
